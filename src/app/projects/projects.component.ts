4636e23d17906625cc31c2a57afc283e069075c1
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-01-03 21:49:01 +0100

    Progress bar

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,10 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  // encapsulation : ViewEncapsulation.None
 })
 
 export class ProjectsComponent {
